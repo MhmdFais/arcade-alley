4610a9f0af7e6d0418abefd983d81c0d7da9a918
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-08-29 17:05:31 +0530

    chore: Update Tailwind CSS configuration, add lazy loading for images, and update background image styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@
 
 function App() {
   return (
-    <></>
+    <>  
+      <h1 className="text-3xl text-yellow-300">Home</h1>
+      <p className="font-mono text-yellow-200">Welcome to the home page</p>
+    </>
   )
 }
 
