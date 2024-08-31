f1917104285f0348e2754f73fd097a167293c00b
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-08-31 12:30:55 +0530

    chore: added rawg api

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,6 +1,34 @@
+import { data } from 'autoprefixer';
 import Navbar from './Components/Navbar'
+import { useState, useEffect } from 'react';
 
 function Shop(){
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [games, setGames] = useState([])
+
+    useEffect(() => {
+        const fetchGames = async () => {
+            try {
+                const response = await fetch('https://api.rawg.io/api/games?key=56bac37d425c4333acc6a0aa86319853')
+                const datas = await response.json()
+                setGames(datas.results)
+                setIsLoading(false)
+            }
+            catch(error){
+                console.log(`Error while fetching`, error)
+                setIsLoading(false)
+            }
+        }
+
+        fetchGames()
+
+        return () => {
+            console.log('Component unmounted');
+        };
+    }, [])
+
+
     return(
         <div className="shopDiv">
             <div className="logoDiv">
