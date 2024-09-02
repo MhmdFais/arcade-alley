8c4a094486476debc8b717c6b7601ce623949f7c
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-09-02 08:58:33 +0530

    feat: Add Navbar component to About page

--- src/About.jsx
+++ src/About.jsx
@@ -1,3 +1,14 @@
-function About(){}
+import Navbar from './Components/Navbar'
+
+function About(){
+    return(
+        <div className='aboutDiv'>
+            <div className="logoDiv">
+                <div className="logo"></div>
+                <Navbar />
+            </div>
+        </div>
+    )
+}
 
 export default About;
